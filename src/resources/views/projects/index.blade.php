7b7b4bd1aa6ffae645168b0aaee0591f214e970d
jaylzv <jaylzv7@gmail.com>
2025-04-12 20:03:42 +0200

    delete about button

--- src/resources/views/projects/index.blade.php
+++ src/resources/views/projects/index.blade.php
@@ -14,19 +14,16 @@
                         </div>
                         <div class="flex flex-col gap-4 flex-grow text-black">
                             <h2 class="text-2xl font-extrabold">{{ $project->name }}</h2>
-                            <div class="flex flex-row justify-between font-bold p-3 rounded-md bg-gray-200"><span>Landlord
+                            <div class="flex flex-row p-3 rounded-md gap-2"><span class="font-bold">Landlord
                                     name:</span><span>{{ $project->landlord->name }}</span></div>
-                            <div class="flex flex-row justify-between font-bold p-3 rounded-md">
-                                <span>Description:</span><span>{{ $project->description }}</span></div>
-                            <div class="flex flex-row justify-between font-bold p-3 rounded-md bg-gray-200"><span>Broj dostupnih
-                                    sadnic:</span><span>{{ $project->amount }}</span></div>
-                            <div class="flex flex-row justify-between font-bold p-3 rounded-md"><span>Cena jedne
-                                    sadnice:</span><span>{{ $project->price }}</span></div>
+                            <div class="flex flex-row p-3 rounded-md gap-2">
+                                <span class="font-bold">Description: </span><span>{{ $project->description }}</span></div>
+                            <div class="flex flex-row p-3 rounded-md gap-2"><span class="font-bold">Broj dostupnih
+                                    sadnic: </span><span>{{ $project->amount }}</span></div>
+                            <div class="flex flex-row p-3 rounded-md  gap-2"><span class="font-bold">Cena jedne
+                                    sadnice: </span><span>{{ $project->price }}</span></div>
                         </div>
                         <div class="flex flex-col flex-shrink-0 justify-evenly gap-4 w-[200px]">
-                            <a class="text-center py-3 rounded-md bg-green-800 hover:bg-green-700 text-white font-bold" href="{{ route('projects.show', $project->id) }}">
-                                About
-                            </a>
                             <button class="py-3 rounded-md bg-green-800 hover:bg-green-700 text-white font-bold">Calculate</button>
                             <button class="py-3 rounded-md bg-green-800 hover:bg-green-700 text-white font-bold">Invest</button>
                         </div>
