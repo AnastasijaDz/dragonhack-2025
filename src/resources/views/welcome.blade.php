bdc27cab9a1ddb7b589804e1dd652b656fbd4ffa
jaylzv <jaylzv7@gmail.com>
2025-04-12 16:12:05 +0200

    add header

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -8,9 +8,40 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-gray-100 text-gray-900">
+<body>
     <div class="min-h-screen flex flex-col">
-        <div class="p-4 min-h-20 bg-green-800"></div>
+
+        <div class="py-4 px-8 min-h-20 bg-green-800 flex flex-row justify-between text-white font-bold">
+            <button class="flex items-center">
+                EkoStock
+            </button>
+
+            <div class="flex flex-row items-center gap-6">
+                <button class="flex items-center">
+                    About us
+                </button>
+                <button class="flex items-center">
+                    Investemnts
+                </button>
+                <button class="flex items-center">
+                    ROI Calculator
+                </button>
+                <button class="flex items-center">
+                    FAQ
+                </button>
+            </div>
+
+            <button class="flex flex-row gap-6 items-center">
+                <img class="rounded-full" src="https://picsum.photos/id/237/64/64" />
+                <span>Aleksa Sibinović</span>
+                <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M17.7463 0H7.26612C2.71385 0 0 2.7125 0 7.2625V17.725C0 22.2875 2.71385 25 7.26612 25H17.7338C22.2861 25 25 22.2875 25 17.7375V7.2625C25.0125 2.7125 22.2986 0 17.7463 0ZM18.7593 19.0625H6.25312C5.74036 19.0625 5.31515 18.6375 5.31515 18.125C5.31515 17.6125 5.74036 17.1875 6.25312 17.1875H18.7593C19.2721 17.1875 19.6973 17.6125 19.6973 18.125C19.6973 18.6375 19.2721 19.0625 18.7593 19.0625ZM18.7593 13.4375H6.25312C5.74036 13.4375 5.31515 13.0125 5.31515 12.5C5.31515 11.9875 5.74036 11.5625 6.25312 11.5625H18.7593C19.2721 11.5625 19.6973 11.9875 19.6973 12.5C19.6973 13.0125 19.2721 13.4375 18.7593 13.4375ZM18.7593 7.8125H6.25312C5.74036 7.8125 5.31515 7.3875 5.31515 6.875C5.31515 6.3625 5.74036 5.9375 6.25312 5.9375H18.7593C19.2721 5.9375 19.6973 6.3625 19.6973 6.875C19.6973 7.3875 19.2721 7.8125 18.7593 7.8125Z"
+                        fill="white" />
+                </svg>
+            </button>
+        </div>
+
     </div>
 </body>
 
