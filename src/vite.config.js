0ed974a1851bf43daf2b197ea9c7a7c97eb3d116
jaylzv <jaylzv7@gmail.com>
2025-04-12 14:23:12 +0200

    add changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
-export default {
-    server: {
-      host: '0.0.0.0', // Allow external access
-      port: 5173, // Ensure this port matches the one in your container
-    },
-  };
-  
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+});
