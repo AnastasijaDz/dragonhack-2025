660fd317a8f61094eed75062adf1c17a7a9d26a2
Jordan Lazov <118006842+jaylzv@users.noreply.github.com>
2025-04-12 21:20:15 +0200

    add footer in pages layout

---
+++ src/resources/views/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="border-t-[1px] border-gray-400 w-full flex flex-row p-8 justify-around mt-20 bg-white">
+  <div class="flex flex-row gap-20">
+    <div class="flex flex-col gap-2">
+      <span class="font-bold text-lg">Contact Us</span>
+      <span>support@ecostock.com</span>
+      <span>+386 1 234 567</span>
+      <span>Ljubljana, Slovenia</span>
+    </div>
+    <div class="flex flex-col gap-2">
+      <span class="font-bold text-lg">Quick Links</span>
+      <a href="#" class="hover:text-green-700">About Us</a>
+      <a href="#" class="hover:text-green-700">Investments</a>
+      <a href="#" class="hover:text-green-700">FAQ</a>
+    </div>
+    <div class="flex flex-col gap-2">
+      <span class="font-bold text-lg">Legal</span>
+      <a href="#" class="hover:text-green-700">Privacy Policy</a>
+      <a href="#" class="hover:text-green-700">Terms of Service</a>
+      <a href="#" class="hover:text-green-700">Cookie Policy</a>
+    </div>
+  </div>
+  <div class="flex flex-col gap-2 items-center justify-center">
+    <span class="text-gray-600">© 2025 EcoStock.</span>
+    <span class="text-gray-600">All rights reserved.</span>
+  </div>
+</footer>
