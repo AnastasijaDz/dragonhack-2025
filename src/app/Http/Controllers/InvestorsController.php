2e67d31c07023535409963f0bc73ce69d6bb4b1a
AnastasijaDz <127784694+AnastasijaDz@users.noreply.github.com>
2025-04-13 04:33:47 +0200

    user profile data
    Co-authored-by: Anastasija <anastasija@pc7.dev>

--- src/app/Http/Controllers/InvestorsController.php
+++ src/app/Http/Controllers/InvestorsController.php
@@ -20,6 +20,11 @@ class InvestorsController extends Controller
     {
         $user = Auth::user();
         $investor = Investor::with('user')->findOrFail($user->profile()->id);
+        // dd($investor->total_invested); // Invested
+        // dd($investor->earned_price); // Earned back
+        // dd($investor->most_profitable_project?->name); // Most profitable project
+        // dd($investor->investments_per_year); // Most profitable project
+        // dd($investor->investment_allocation); // Investment allocation
         return view('investors.show', compact('investor'));
     }
 
